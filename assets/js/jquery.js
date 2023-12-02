c9d38e183c23f94f94f8e73f4ddd26909520edce
muhammadmasud1 <mdmasudrana15020@gmail.com>
2023-12-02 19:19:39 +0600

    new update

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -22,14 +22,9 @@
 		factory( global );
 	}
 
-// Pass this if window is not defined yet
+
 }(typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
 
-// Support: Firefox 18+
-// Can't be in strict mode, several libs including ASP.NET trace
-// the stack via arguments.caller.callee and Firefox dies if
-// you try to trace through "use strict" call chains. (#13335)
-//"use strict";
 var arr = [];
 
 var document = window.document;
